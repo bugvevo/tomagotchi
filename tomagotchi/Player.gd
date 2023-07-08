2131de552e16f46178397f3d5344b8f22e34ab1b
Isabel Skidmore <isabels@wolfram.com>
2023-07-08 19:07:58 -0400

    Add assets and create Main and Player scenes. Create player movement with click animation.

---
+++ tomagotchi/Player.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var screenSize
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screenSize = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var mousePos = get_viewport().get_mouse_position()
+	position.x = clamp(mousePos.x + 50, 75, screenSize.x - 75)
+	position.y = clamp(max(mousePos.y, 200) + 130, 0, screenSize.y)
+
+func _input(event):
+   # Mouse in viewport coordinates.
+	if event is InputEventMouseButton && $AnimatedSprite2D.frame == 0:
+		$AnimatedSprite2D.frame = 1
+	elif event is InputEventMouseButton && $AnimatedSprite2D.frame == 1:
+		$AnimatedSprite2D.frame = 0
